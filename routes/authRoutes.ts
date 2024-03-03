ab237219cd3ec6f2d02ea8b500247f2de9154b94
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-03 12:50:56 +0200

    Make the auth routes

---
+++ routes/authRoutes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { register, login } from "../controllers/authControllers";
+
+export const authRouter = express.Router();
+
+authRouter.post("/register", register);
+authRouter.post("/login", login);
